0a2009b234dd743d1619df58ef8a32a966cf94bf
DarkMatterMatt <mattmoran2000@gmail.com>
2018-09-12 10:18:38 +1200

    Temporary testing

--- library/register.js
+++ library/register.js
@@ -92,6 +92,7 @@ formElem.addEventListener("submit", function(ev) {
     // Populate hidden home address fields
     var homePlace = homeAutocomplete.getPlace();
     var homeComponents = getComponentsByTypeObject(homePlace.address_components);
+    console.log(homeComponents);
     var homeSplitAddress = homeAddressElem.value.split(", ");
     homeStreetElemForServer.value = homeSplitAddress[0];
     homeSuburbElemForServer.value = homeSplitAddress[1];
