2f5bc1f6f46bb217c2536fcb5fe73bbd3b5a2336
DarkMatterMatt <mattmoran2000@gmail.com>
2018-12-24 15:13:40 +1300

    add columnNames constant
    Everything now loops through the columns instead of having a seperate line for each column

--- gliding/index.js
+++ gliding/index.js
@@ -1,27 +1,16 @@
 "use strict";
 
+const columnNames = ["date", "flightNumber", "pilotInCommand", "studentPassenger", "glider", "towReg", "towPilot", "takeOff", "towDown", "towHeight", "gliderDown", "towTime", "gliderTime", "towCost", "gliderCost", "landingFee", "totalCost", "comments"];
+
 const $window = $(window);
-const $date = $(".date").first();
-const $flightNumber = $(".num").first();
-const $pilotInCommand = $(".pic").first();
-const $studentPassenger = $(".passenger").first();
-const $glider = $(".glider-reg").first();
-const $towReg = $(".tow-reg").first();
-const $towPilot = $(".tow-pilot").first();
-const $takeOff = $(".take-off").first();
-const $towDown = $(".tow-down").first();
-const $towHeight = $(".tow-height").first();
-const $gliderDown = $(".glider-down").first();
-const $towTime = $(".tow-time").first();
-const $gliderTime = $(".glider-time").first();
-const $towCost = $(".tow-cost").first();
-const $gliderCost = $(".glider-cost").first();
-const $landingFee = $(".landing-fee").first();
-const $totalCost = $(".total-cost").first();
-const $comments = $(".comments").first();
+const $elems = {};
+for (let i = 0; i < columnNames.length; i++) {
+    const col = columnNames[i];
+    $elems[col] = $("." + col).first();
+}
 
 function localStorageTest() {
-    var test = "test";
+    const test = "test";
     try {
         localStorage.setItem(test, test);
         localStorage.removeItem(test);
@@ -33,41 +22,30 @@ function localStorageTest() {
 const localStorageEnabled = localStorageTest();
 
 function rowIsEmpty(row) {
+    // make a copy of the row without the flightNumber or date columns
     row = Object.assign({}, row);
     delete row.date;
     delete row.flightNumber;
-    return Object.values(row).every(function (value) {
-        return value === "";
-    });
+
+    // if every column is blank then row is empty
+    return Object.values(row).every(v => v === "");
 }
 
 function getRow(index) {
-    return {
-        "date": $date.find("input").eq(index).val(),
-        "flightNumber": $flightNumber.find("input").eq(index).val(),
-        "pilotInCommand": $pilotInCommand.find("input").eq(index).val(),
-        "studentPassenger": $studentPassenger.find("input").eq(index).val(),
-        "glider": $glider.find("input").eq(index).val(),
-        "towReg": $towReg.find("input").eq(index).val(),
-        "towPilot": $towPilot.find("input").eq(index).val(),
-        "takeOff": $takeOff.find("input").eq(index).val(),
-        "towDown": $towDown.find("input").eq(index).val(),
-        "towHeight": $towHeight.find("input").eq(index).val(),
-        "gliderDown": $gliderDown.find("input").eq(index).val(),
-        "towTime": $towTime.find("input").eq(index).val(),
-        "gliderTime": $gliderTime.find("input").eq(index).val(),
-        "towCost": $towCost.find("input").eq(index).val(),
-        "gliderCost": $gliderCost.find("input").eq(index).val(),
-        "landingFee": $landingFee.find("input").eq(index).val(),
-        "totalCost": $totalCost.find("input").eq(index).val(),
-        "comments": $comments.find("input").eq(index).val(),
-    };
+    const row = {};
+    for (let i = 0; i < columnNames.length; i++) {
+        const col = columnNames[i];
+        const val = $elems[col].find("input").eq(index).val();
+        if (val) {
+            row[col] = val;
+        }
+    }
+    return row;
 }
 
 function exportToString() {
     const rows = [];
-    const numRows = $date.find("input").length;
-    for (let i = 0; i < numRows; i++) {
+    for (let i = 0; i < columnNames.length; i++) {
         const row = getRow(i);
         if (!rowIsEmpty(row)) {
             rows.push(row);
@@ -77,43 +55,12 @@ function exportToString() {
 }
 
 function addRow(row) {
-    $date.append($("<div><input>").addClass("input-wrapper"));
-    $flightNumber.append($("<div><input>").addClass("input-wrapper"));
-    $pilotInCommand.append($("<div><input>").addClass("input-wrapper"));
-    $studentPassenger.append($("<div><input>").addClass("input-wrapper"));
-    $glider.append($("<div><input>").addClass("input-wrapper"));
-    $towReg.append($("<div><input>").addClass("input-wrapper"));
-    $towPilot.append($("<div><input>").addClass("input-wrapper"));
-    $takeOff.append($("<div><input>").addClass("input-wrapper"));
-    $towDown.append($("<div><input>").addClass("input-wrapper"));
-    $towHeight.append($("<div><input>").addClass("input-wrapper"));
-    $gliderDown.append($("<div><input>").addClass("input-wrapper"));
-    $towTime.append($("<div><input>").addClass("input-wrapper"));
-    $gliderTime.append($("<div><input>").addClass("input-wrapper"));
-    $towCost.append($("<div><input>").addClass("input-wrapper"));
-    $gliderCost.append($("<div><input>").addClass("input-wrapper"));
-    $landingFee.append($("<div><input>").addClass("input-wrapper"));
-    $totalCost.append($("<div><input>").addClass("input-wrapper"));
-    $comments.append($("<div><input>").addClass("input-wrapper"));
-
-    $date.find("input").last().val(row.date);
-    $flightNumber.find("input").last().val(row.flightNumber);
-    $pilotInCommand.find("input").last().val(row.pilotInCommand);
-    $studentPassenger.find("input").last().val(row.studentPassenger);
-    $glider.find("input").last().val(row.glider);
-    $towReg.find("input").last().val(row.towReg);
-    $towPilot.find("input").last().val(row.towPilot);
-    $takeOff.find("input").last().val(row.takeOff);
-    $towDown.find("input").last().val(row.towDown);
-    $towHeight.find("input").last().val(row.towHeight);
-    $gliderDown.find("input").last().val(row.gliderDown);
-    $towTime.find("input").last().val(row.towTime);
-    $gliderTime.find("input").last().val(row.gliderTime);
-    $towCost.find("input").last().val(row.towCost);
-    $gliderCost.find("input").last().val(row.gliderCost);
-    $landingFee.find("input").last().val(row.landingFee);
-    $totalCost.find("input").last().val(row.totalCost);
-    $comments.find("input").last().val(row.comments);
+    if (!row) row = {};
+    for (let i = 0; i < columnNames.length; i++) {
+        const col = columnNames[i];
+        $elems[col].append($("<div><input>").addClass("input-wrapper"));
+        $elems[col].find("input").last().val(row[col] || "");
+    }
 }
 
 function saveToLocalStorage() {
@@ -135,40 +82,16 @@ function loadFromLocalStorage(storageName) {
 }
 
 function getLastFlightNumber() {
-    const $input = $flightNumber.find("input").eq(-2);
+    const $input = $elems.flightNumber.find("input").eq(-1);
     if ($input.length === 0) return 0;
     return parseInt($input.val());
 }
 
-function addBlankRow() {
-    $date.append($("<div><input>").addClass("input-wrapper"));
-    $flightNumber.append($("<div><input>").addClass("input-wrapper"));
-    $pilotInCommand.append($("<div><input>").addClass("input-wrapper"));
-    $studentPassenger.append($("<div><input>").addClass("input-wrapper"));
-    $glider.append($("<div><input>").addClass("input-wrapper"));
-    $towReg.append($("<div><input>").addClass("input-wrapper"));
-    $towPilot.append($("<div><input>").addClass("input-wrapper"));
-    $takeOff.append($("<div><input>").addClass("input-wrapper"));
-    $towDown.append($("<div><input>").addClass("input-wrapper"));
-    $towHeight.append($("<div><input>").addClass("input-wrapper"));
-    $gliderDown.append($("<div><input>").addClass("input-wrapper"));
-    $towTime.append($("<div><input>").addClass("input-wrapper"));
-    $gliderTime.append($("<div><input>").addClass("input-wrapper"));
-    $towCost.append($("<div><input>").addClass("input-wrapper"));
-    $gliderCost.append($("<div><input>").addClass("input-wrapper"));
-    $landingFee.append($("<div><input>").addClass("input-wrapper"));
-    $totalCost.append($("<div><input>").addClass("input-wrapper"));
-    $comments.append($("<div><input>").addClass("input-wrapper"));
-
-    $date.find("input").last().val(moment().format("DD MMM"));
-    $flightNumber.find("input").last().val(getLastFlightNumber() + 1);
-}
-
 function addTimeToElem(ev) {
     const $target = $(ev.target);
-    if ($takeOff.has($target).length || $towDown.has($target).length || $gliderDown.has($target).length) {
+    if ($elems.takeOff.has($target).length || $elems.towDown.has($target).length || $elems.gliderDown.has($target).length) {
         $target.val(moment().format("HH:mm"));
-        $target.trigger("keypress");
+        $target.trigger("keyup");
     }
 }
 
@@ -182,11 +105,17 @@ $window.on("keyup", ev => {
         saveToLocalStorage();
     }
     if (!rowIsEmpty(getRow(-1))) {
-        addBlankRow();
+        addRow({
+            "date": moment().format("DD MMM"),
+            "flightNumber": getLastFlightNumber() + 1
+        });
     }
 });
 
 if (localStorageEnabled) {
     loadFromLocalStorage("latest");
 }
-addBlankRow();
+addRow({
+    "date": moment().format("DD MMM"),
+    "flightNumber": getLastFlightNumber() + 1
+});
